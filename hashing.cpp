e28f3ecbff239cbab374cb71002a1b1a7a66fe83
Saksham Khatri <saksham.khatri3103@gmail.com>
2025-07-02 21:59:16 +0530

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -106,27 +106,6 @@ how many times 4
 // return 0;
 // }
 
-#include<bits/stdc++.h>
-using namespace std;
 
-int main()
-{
-    int n;
-    cin>>n;
-    int num[n];
-    for(int i=0; i<n; i++){
-        cin>>num[i];
-    }
-   map<int,int>mpp;
-   for(int i=0; i<n; i++){
-    mpp[num[i]]++;
-   }
 
-   for(auto it : mpp){
-    cout<<"["<<it.first<<","<<it.second<<"]"<<"";
-   }
-
-   
-
-return 0;
-}
+  
